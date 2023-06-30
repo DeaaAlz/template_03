31f90305ab1664d6cec29bcabdebea2cbaddf3df
DeaaAlz <deaaldeen409@gmail.com>
2023-06-30 19:36:40 +0300

    modify css file and add width smooth for progress

--- js/main.js
+++ js/main.js
@@ -27,3 +27,15 @@ let counter = setInterval(() => {
     clearInterval(counter);
   }
 }, 1000);
+
+let section = document.querySelector(".our-skills");
+let spans = document.querySelectorAll(".the-progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    console.log("Reached Section Three");
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
